847c41e802d371f32afd7791f24ae3e726a848dc
norma <normandojefferson@hotmail.com>
2024-06-30 17:04:01 -0300

    branch atividade-mapa

--- demo/src/main/java/com/mapa/HelloWorld.java
+++ demo/src/main/java/com/mapa/HelloWorld.java
@@ -3,5 +3,6 @@ package com.mapa;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Olá, mundo!");
+        System.out.println("atividade-mapa");
     }
 }
